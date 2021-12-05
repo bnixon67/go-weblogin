b22c672e41fbffea92e0c1e7dc0fbc7805d07f91
Bill Nixon <bnixon67@gmail.com>
2021-12-05 16:26:11 -0600

    moved App from main

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,56 +9,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-type App struct {
-	db     *sql.DB
-	tmpls  *template.Template
-	config Config
-}
-
-func NewApp(configFileName, logFileName string) (*App, error) {
-	var app App
-	var err error
-
-	err = InitLogging(logFileName)
-	if err != nil {
-		return &app, err
-	}
-
-	// read config file
-	app.config, err = NewConfigFromFile(configFileName)
-	if err != nil {
-		log.Printf("unable to read config file %q, %v", configFileName, err)
-		return &app, err
-	}
-
-	// ensure required config values have been provided
-	if !app.config.IsValid() {
-		log.Printf("config is not valid")
-		return &app, err
-	}
-
-	// TODO: handle this default value
-	if app.config.SessionExpiresHours == 0 {
-		app.config.SessionExpiresHours = 24
-	}
-
-	// init database connection
-	app.db, err = initDB(app.config.SQLDriverName, app.config.SQLDataSourceName)
-	if err != nil {
-		log.Printf("initDB failed: %v", err)
-		return &app, err
-	}
-
-	// init HTML templates
-	app.tmpls, err = initTemplates(app.config.ParseGlobPattern)
-	if err != nil {
-		log.Printf("initTemplates failed: %v", err)
-		return &app, err
-	}
-
-	return &app, err
-}
 
 // main function
 func main() {
@@ -96,6 +44,7 @@ func main() {
 	http.HandleFunc("/reset", app.ResetHandler)
 	http.HandleFunc("/hello", app.HelloHandler)
 	http.Handle("/style.css", http.FileServer(http.Dir("html")))
+	http.Handle("/w3.css", http.FileServer(http.Dir("html")))
 	http.Handle("/", http.RedirectHandler("/hello", http.StatusMovedPermanently))
 
 	// run server
