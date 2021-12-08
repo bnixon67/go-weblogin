8a4b61a1696cdabde8d676b7263f94cffaa9f8bb
Bill Nixon <bnixon67@gmail.com>
2021-12-07 21:40:30 -0600

    generate test cases for ConfigIsValid based on required fields

--- config_test.go
+++ config_test.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"math"
+	"reflect"
 	"testing"
 )
+
+func hasBit(n int, pos uint) bool {
+	val := n & (1 << pos)
+	return (val > 0)
+}
 
 func TestConfigIsValid(t *testing.T) {
 	InitLogging("test.log")
 
-	// TODO: generate programmatically based on required values
-	cases := []struct {
+	type tcase struct {
 		config   Config
 		expected bool
-	}{
-		{Config{ServerHost: "", SQLDriverName: "", SQLDataSourceName: "", ParseGlobPattern: ""}, false},
-		{Config{ServerHost: "", SQLDriverName: "", SQLDataSourceName: "", ParseGlobPattern: "foo"}, false},
-		{Config{ServerHost: "", SQLDriverName: "", SQLDataSourceName: "foo", ParseGlobPattern: ""}, false},
-		{Config{ServerHost: "", SQLDriverName: "", SQLDataSourceName: "foo", ParseGlobPattern: "foo"}, false},
-		{Config{ServerHost: "foo", SQLDriverName: "foo", SQLDataSourceName: "", ParseGlobPattern: ""}, false},
-		{Config{ServerHost: "foo", SQLDriverName: "foo", SQLDataSourceName: "", ParseGlobPattern: "foo"}, false},
-		{Config{ServerHost: "foo", SQLDriverName: "foo", SQLDataSourceName: "foo", ParseGlobPattern: ""}, false},
-		{Config{ServerHost: "foo", SQLDriverName: "foo", SQLDataSourceName: "foo", ParseGlobPattern: "foo"}, false},
-		{Config{ServerHost: "foo", ServerPort: "foo", SQLDriverName: "foo", SQLDataSourceName: "foo", ParseGlobPattern: "foo"}, true},
 	}
+	var cases []tcase
+
+	// required fields
+	required := []string{"ServerHost", "ServerPort", "SQLDriverName", "SQLDataSourceName", "ParseGlobPattern"}
+
+	// generate test cases based on required fields by looping thru all the possibilities and using bit logic to set fields
+	for a := 0; a < int(math.Pow(2, float64(len(required)))); a++ {
+		config := Config{}
+		for n := len(required) - 1; n >= 0; n-- {
+			if hasBit(a, uint(n)) {
+				reflect.ValueOf(&config).Elem().FieldByName(required[n]).SetString("x")
+			}
+		}
+		cases = append(cases, tcase{config, false})
+	}
+	// last case should be true since all required fields are present
+	cases[len(cases)-1].expected = true
 
 	for _, testCase := range cases {
 		got := testCase.config.IsValid()
