ff2a14474144cc7332cd1ad46e0cb0b59db724fc
Bill Nixon <bnixon67@gmail.com>
2022-01-29 14:51:09 -0600

    fixed path to static html files

--- weblogin-server/main.go
+++ weblogin-server/main.go
@@ -63,8 +63,8 @@ func main() {
 	http.HandleFunc("/reset", app.ResetHandler)
 	http.HandleFunc("/hello", app.HelloHandler)
 	// TODO: define base html directory in config
-	http.HandleFunc("/w3.css", weblogin.ServeFileHandler("../../html/w3.css"))
-	http.HandleFunc("/favicon.ico", weblogin.ServeFileHandler("../../html/favicon.ico"))
+	http.HandleFunc("/w3.css", weblogin.ServeFileHandler("../html/w3.css"))
+	http.HandleFunc("/favicon.ico", weblogin.ServeFileHandler("../html/favicon.ico"))
 	http.Handle("/",
 		http.RedirectHandler("/hello", http.StatusMovedPermanently))
 
